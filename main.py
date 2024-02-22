7ccce601dae86c4496ad6cd1e3b0a11699f4e54b
Sandeep Pillai <sandeeppillai@Sandeeps-MacBook-Air.local>
2024-02-22 22:03:16 +0530

    1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from models import SourceFilterMixtureAutoencoder2
 from utils import load_model
 
 # Loading Model
-tag = "unsupervised_2s_CSD_mf0_1"
+tag = "unsupervised_2s_satb_bcbq_mf0_1"
 umss_model = load_model(tag)
 print("Model Params: ", umss_model)
