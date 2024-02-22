fe360efadac800e14dab13b075f16bf628d56e91
Sandeep Pillai <sandeeppillai@Sandeeps-MacBook-Air.local>
2024-02-22 22:31:30 +0530

    commit

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
+import torch
+import torchaudio
+from torchaudio.transforms import Resample
 from models import SourceFilterMixtureAutoencoder2
 from utils import load_model
 
 # Loading Model
 tag = "unsupervised_2s_satb_bcbq_mf0_1"
 umss_model = load_model(tag)
-print("Model Params: ", umss_model)
+print("Model Params: ", umss_model, "\n")
+
+# Loading mixed audio file
+audio_file = "./Datasets/ChoralSingingDataset/Nino_Dios/mixtures_2_sources/mixed_audio/nino_bajos104_sop_105.wav"
+mixed_audio, sample_rate = torchaudio.load(audio_file)
+
+resample = Resample(orig_freq=sample_rate, new_freq=16000) # architecture is designed for signals sampled at 16kHz
+mixed_audio_resampled = resample(mixed_audio)
+
+input_audio = mixed_audio_resampled.unsqueeze(0) # Expand dimensions to match the expected shape [batch_size, n_samples]
+
+# Loading the F0 information of nino_dios_Sno_105_Bos_104.pt file
+f0_file = "./Datasets/ChoralSingingDataset/Nino_Dios/mixtures_2_sources/mf0_cuesta_processed/nino_dios_Sno_105_Bos_104.pt"
+input_f0 = torch.load(f0_file)
+input_f0 = input_f0.unsqueeze(0) #Ensuring the shape is appropriate for your model ([batch_size, n_freq_frames, n_sources])
+
+# Perform inference
+with torch.no_grad():
+    output = umss_model(input_audio, input_f0)
+
+# Process output
+if isinstance(output, tuple):
+    mix, sources = output
+    # Process separated sources as needed
+    print("Separated sources shape:", sources.shape)
+else:
+    mix = output
+    print("Mixed audio shape:", mix.shape)
