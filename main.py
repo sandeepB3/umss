700617a92de7eeb636dc9f463deff963c3ed94f3
Sandeep Pillai <sandeeppillai@Sandeeps-MacBook-Air.local>
2024-02-22 18:47:04 +0530

    commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import librosa, librosa.display
 from IPython.display import Audio
 import torch
 import torch.nn as nn
-from models import Base_model, F0Extractor, SourceFilterMixtureAutoencoder2
+from utils import load_model
 
 audio1 = "./Datasets/ChoralSingingDataset/Nino_Dios/audio_16kHz/nino_Bajos_104.wav"
 audio2 = "./Datasets/ChoralSingingDataset/Nino_Dios/audio_16kHz/nino_Soprano_105.wav"
@@ -16,13 +16,7 @@ Audio(data=signal1, rate=sample_rate1)
 # Play Audio 2
 Audio(data=signal2, rate=sample_rate2)
 
-# Network Dict Path for F0Extractor Model
-network_dict_path = "./Datasets/ChoralSingingDataset/Nino_Dios/mixtures_2_sources/mf0_cuesta_processed/nino_dios_Sno_105_Bos_104.pt"
-
-# Network Dict Path for SourceFilterMixtureAutoencoder2 Model
-network_dict_path = "./trained_models/unsupervised_2s_satb_bcbq_mf0_1/unsupervised_2s_satb_bcbq_mf0_1.pth"
-
-# Loading the SourceFilterMixtureAutoencoder2 Model
-sourceFilterMixtureAutoEncoder2 = SourceFilterMixtureAutoencoder2()
-state_dict = torch.load(network_dict_path)
-sourceFilterMixtureAutoEncoder2.load_state_dict(state_dict)
+# Loading Model
+tag = "unsupervised_2s_CSD_mf0_1"
+umss_model = load_model(tag)
+print("Model Params: ", umss_model)
